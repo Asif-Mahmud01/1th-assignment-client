1792319242b5112ffea69dfbd04dc73d23c4b134
Asif-Mahmud01 <asifmahmud3.am@gmail.com>
2022-11-11 23:15:10 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -15,17 +15,92 @@ console.log(process.env.DB_PASSWORD)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zbisngo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('photography').collection('Services')
+        const orderCollection = client.db('geniusCar').collection('orders')
+
+        app.get('/services', async(req, res)=>{
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        app.get('/service', async(req, res)=>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        // app.get('/services', async(req, res)=>{
+        //     const query = {}
+        //     const cursor = serviceCollection.find(query)
+        //     const services = await cursor.toArray()
+        //     res.send(services)
+        // })
+
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+        // orders api
+
+        app.get('/orders', async(req, res) =>{
+            let query = {}
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query)
+            const orders = await cursor.toArray()
+            res.send(orders)
+        })
+
+        app.patch('/orders/:id', async(req, res) =>{
+            const id = req.params.id;
+            const status = req.body.status
+            const query = {_id: ObjectId(id)}
+            const updatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await orderCollection.updateOne(query, updatedDoc)
+            res.send(result)
+        })
+
+        app.delete('/orders/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.post('/orders', async(req, res) =>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err))
 
 
 app.get('/', (req, res) =>{
-    res.send('genius car server is running ')
+    res.send('Photography server is running ')
 })
 
 app.listen(port, ()=>{
-    console.log(`genius car server is running ${port}`)
+    console.log(`photography server is running ${port}`)
 })
